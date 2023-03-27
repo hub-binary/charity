49410a8dad4ad119de42741c6dbcc59e7fc5c236
Joel Tanko <7thogofe@gmail.com>
2023-03-27 11:05:07 +0100

    Update Readme

--- app/donate/page.jsx
+++ app/donate/page.jsx
@@ -1,357 +1,87 @@
-// 'use-client'
-// import {Button} from '@chakra-ui/next-js'
+'use client'
+import QRCode from "qrcode.react";
 import Image from 'next/image'
 // import styles from './page.module.css'
 
 
 export default function Home() {
-  return (
+  return(
     <div className="">
-      <div class="fh5co-hero">
-        <div class="fh5co-overlay"></div>
-        <div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style={{}}>
-          <div class="desc animate-box">
-            <h2><strong> Help </strong> <strong> Poor Children </strong> </h2>
-
-
-            <span class="mt-3"><a class="btn btn-primary btn-lg" href="#">Donate Now</a></span>
-          </div>
-        </div>
-      </div>
       
-      {/* end:header-top */}
-      <div id="fh5co-features">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4">
-
-              <div class="feature-left">
-                <span class="icon">
-                  <i class="icon-profile-male"></i>
-                </span>
-                <div class="feature-copy">
-                  <h3>Become a volunteer</h3>
-                  <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
-                  <p><a href="#">Learn More</a></p>
-                </div>
-              </div>
-
-            </div>
-
-            <div class="col-md-4">
-              <div class="feature-left">
-                <span class="icon">
-                  <i class="icon-happy"></i>
-                </span>
-                <div class="feature-copy">
-                  <h3>Happy Giving</h3>
-                  <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
-                  <p><a href="#">Learn More</a></p>
-                </div>
-              </div>
-
-            </div>
-            <div class="col-md-4">
-              <div class="feature-left">
-                <span class="icon">
-                  <i class="icon-wallet"></i>
-                </span>
-                <div class="feature-copy">
-                  <h3>Donation</h3>
-                  <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit.</p>
-                  <p><a href="#">Learn More</a></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div id="fh5co-feature-product" class="fh5co-section-gray">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12 text-center heading-section">
-              <h3>Giving is Virtue.</h3>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-            </div>
-          </div>
-
-          <div class="row row-bottom-padded-md">
-            <div class="col-md-12 text-center animate-box">
-              <p><img src="/assets/images/cover_bg_1.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive" /></p>
-            </div>
-            <div class="col-md-6 text-center animate-box">
-              <p><img src="/assets/images/cover_bg_2.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive" /></p>
-            </div>
-            <div class="col-md-6 text-center animate-box">
-              <p><img src="/assets/images/cover_bg_3.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive" /></p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-4">
-              <div class="feature-text">
-                <h3>Love</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="feature-text">
-                <h3>Compassion</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="feature-text">
-                <h3>Charity</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-            </div>
-          </div>
-
-          
-        </div>
-      </div>
-
-      
-      <div id="fh5co-portfolio">
-        <div class="container">
-
-          <div class="row">
-            <div class="col-md-6 col-md-offset-3 text-center heading-section animate-box">
-              <h3>Our Gallery</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
-            </div>
-          </div>
-
-          
-          <div class="row row-bottom-padded-md">
-            <div class="col-md-12">
-              <ul id="fh5co-portfolio-list">
-
-                <li class="two-third animate-box" data-animate-effect="fadeIn" style={{backgroundImage: "url(/assets/images/cover_bg_1.jpg)"}}>
-                  <a href="#" class="color-3">
-                    <div class="case-studies-summary">
-                      <span>Give Love</span>
-                      <h2>Donation is caring</h2>
-                    </div>
-                  </a>
-                </li>
-              
-                <li class="one-third animate-box" data-animate-effect="fadeIn" style={{backgroundImage: "url(/assets/images/cover_bg_2.jpg)"}}>
-                  <a href="#" class="color-4">
-                    <div class="case-studies-summary">
-                      <span>Give Love</span>
-                      <h2>Donation is caring</h2>
-                    </div>
-                  </a>
-                </li>
-
-                <li class="one-third animate-box" data-animate-effect="fadeIn" style={{backgroundImage: "url(/assets/images/cover_bg_3.jpg)"}}> 
-                  <a href="#" class="color-5">
-                    <div class="case-studies-summary">
-                      <span>Give Love</span>
-                      <h2>Donation is caring</h2>
-                    </div>
-                  </a>
-                </li>
-                <li class="two-third animate-box" data-animate-effect="fadeIn" style={{backgroundImage: "url(/assets/images/cover_bg_3.jpg)"}}>
-                  <a href="#" class="color-6">
-                    <div class="case-studies-summary">
-                      <span>Give Love</span>
-                      <h2>Donation is caring</h2>
-                    </div>
-                  </a>
-                </li>
-              </ul>   
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-4 col-md-offset-4 text-center animate-box">
-              <a href="#" class="btn btn-primary btn-lg">See Gallery</a>
-            </div>
-          </div>
-
-          
-        </div>
-      </div>
-      
-
-      
-      <div id="fh5co-content-section" class="fh5co-section-gray">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
-              <h3>Our Philantrophist</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
-            </div>
-          </div>
-        </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="fh5co-testimonial text-center animate-box">
-                <figure>
-                  <img src="/assets/images/person_1.jpg" alt="user" />
-                </figure>
-                <blockquote>
-                  <p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.&rdquo;</p>
-                </blockquote>
-                <span>Jean Doe, XYZ Co.</span>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="fh5co-testimonial text-center animate-box">
-                <figure>
-                  <img src="/assets/images/person_2.jpg" alt="user" />
-                </figure>
-                <blockquote>
-                  <p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.&rdquo;</p>
-                </blockquote>
-                <span>John Doe, XYZ Co.</span>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="fh5co-testimonial text-center animate-box">
-                <figure>
-                  <img src="/assets/images/person_3.jpg" alt="user" />
-                </figure>
-                <blockquote>
-                  <p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.&rdquo;</p>
-                </blockquote>
-                <span>John Doe, XYZ Co.</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* fh5co-content-section */}
-
-      <div id="fh5co-services-section">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
-              <h3>Our Projects. Support Us</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
-            </div>
-          </div>
-        </div>
-        <div class="container">
-          <div class="row text-center">
-            <div class="col-md-4 col-sm-4">
-              <div class="services animate-box">
-                <span><i class="icon-heart"></i></span>
-                <h3>Water Project In Kenya</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-4">
-              <div class="services animate-box">
-                <span><i class="icon-heart"></i></span>
-                <h3>Shelter Giving</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-4">
-              <div class="services animate-box">
-                <span><i class="icon-heart"></i></span>
-                <h3>Shelter Giving</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-4">
-              <div class="services animate-box">
-                <span><i class="icon-heart"></i></span>
-                <h3>Water Project In Kenya</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-4">
-              <div class="services animate-box">
-                <span><i class="icon-heart"></i></span>
-                <h3>Water Project In Kenya</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-            </div>
-            <div class="col-md-4 col-sm-4">
-              <div class="services animate-box">
-                <span><i class="icon-heart"></i></span>
-                <h3>Midical Mission</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* END What we do */}
-
-
-      <div id="fh5co-blog-section" class="fh5co-section-gray">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
-              <h3>Recent From Blog</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis accusamus asperiores sint consequuntur debitis.</p>
-            </div>
-          </div>
-        </div>
-        <div class="container">
-          <div class="row row-bottom-padded-md">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-              <div class="fh5co-blog animate-box">
-                <a href="#"><img class="img-responsive" src="/assets/images/cover_bg_1.jpg" alt="" /></a>
-                <div class="blog-text">
-                  <div class="prod-title">
-                    <h3><a href="#">Medical Mission in Southern Kenya</a></h3>
-                    <span class="posted_by">Sep. 15th</span>
-                    <span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    <p><a href="#">Learn More...</a></p>
-                  </div>
-                </div> 
-              </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6">
-              <div class="fh5co-blog animate-box">
-                <a href="#"><img class="img-responsive" src="/assets/images/cover_bg_2.jpg" alt="" /></a>
-                <div class="blog-text">
-                  <div class="prod-title">
-                    <h3><a href="##">Medical Mission in Southern Kenya</a></h3>
-                    <span class="posted_by">Sep. 15th</span>
-                    <span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    <p><a href="#">Learn More...</a></p>
-                  </div>
-                </div> 
-              </div>
-            </div>
-            <div class="clearfix visible-sm-block"></div>
-            <div class="col-lg-4 col-md-4 col-sm-6">
-              <div class="fh5co-blog animate-box">
-                <a href="#"><img class="img-responsive" src="/assets/images/cover_bg_3.jpg" alt="" /></a>
-                <div class="blog-text">
-                  <div class="prod-title">
-                    <h3><a href="#">Medical Mission in Southern Kenya</a></h3>
-                    <span class="posted_by">Sep. 15th</span>
-                    <span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                    <p><a href="#">Learn More...</a></p>
-                  </div>
-                </div> 
-              </div>
-            </div>
-            <div class="clearfix visible-md-block"></div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-4 col-md-offset-4 text-center animate-box">
-              <a href="#" class="btn btn-primary btn-lg">Our Blog</a>
-            </div>
-          </div>
-
-        </div>
-      </div>
     </div>
   )
 }
+
+const causes = [
+  { name: "Cause 1", value: "cause1" },
+  { name: "Cause 2", value: "cause2" },
+  { name: "Cause 3", value: "cause3" },
+  { name: "Cause 4", value: "cause4" },
+  { name: "Cause 5", value: "cause5" },
+  { name: "Cause 6", value: "cause6" },
+];
+
+const BTCDonation = () => {
+  const [amount, setAmount] = useState(null);
+  const [cause, setCause] = useState(null);
+  const [showQR, setShowQR] = useState(false);
+
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const handleCauseChange = (event) => {
+    setCause(event.target.value);
+  };
+
+  const handleDonateClick = () => {
+    if (amount && cause) {
+      setShowQR(true);
+    }
+  };
+
+  return (
+    <div>
+      {showQR ? (
+        <div>
+          <p>Scan this QR code to donate:</p>
+          <QRCode value={`bitcoin:${process.env.REACT_APP_BTC_ADDRESS}?amount=${amount}&message=${cause}`} />
+          <p>Notes:</p>
+          <ul>
+            <li>Make sure to copy the exact amount and message to avoid any issues.</li>
+            <li>Don't forget to include the transaction fee.</li>
+          </ul>
+        </div>
+      ) : (
+        <div>
+          <p>Select the amount you want to donate:</p>
+          <div>
+            <button onClick={() => setAmount(1)}>1 BTC</button>
+            <button onClick={() => setAmount(0.5)}>0.5 BTC</button>
+            <button onClick={() => setAmount(0.1)}>0.1 BTC</button>
+          </div>
+          <p>Select the cause you want to donate for:</p>
+          <div>
+            {causes.map((cause) => (
+              <div key={cause.value}>
+                <input
+                  type="radio"
+                  id={cause.value}
+                  name="cause"
+                  value={cause.value}
+                  checked={cause.value === cause}
+                  onChange={handleCauseChange}
+                />
+                <label htmlFor={cause.value}>{cause.name}</label>
+              </div>
+            ))}
+          </div>
+          <button onClick={handleDonateClick}>Donate</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BTCDonation;
+
